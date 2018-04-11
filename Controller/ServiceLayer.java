2e54939abeda5937612bbbc1184accbf98f80371
Zeke Thao <zekethao@Zekes-MacBook-Pro.local>
2018-04-10 19:58:32 -0500

    updated comments

--- Controller/ServiceLayer.java
+++ Controller/ServiceLayer.java
@@ -2,5 +2,7 @@ package Controller;
 
 public class ServiceLayer {
 	//All interactions with the UI and the backend need to go through this class
+	//Do validations here as well
+	//try to keep keep UI clean and free of computational logic
 
 }
