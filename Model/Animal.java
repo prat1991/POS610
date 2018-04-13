1363188c33971a9ee439aa952605b6ecd196ce74
ezekielthao <ezekiel.thao2@gmail.com>
2018-04-13 18:33:55 -0500

    added dates to animal

--- Model/Animal.java
+++ Model/Animal.java
@@ -1,9 +1,13 @@
 package Model;
 
 import java.math.BigDecimal;
+import java.sql.Date;
 
 public abstract class Animal {
 	//TODO this is the parent class of all other animals
 	private int UID; //Unique ID for this pet
 	private BigDecimal price; //cost of this pet
+	private Date dateSold;
+	private Date dateStocked;
+	
 }
