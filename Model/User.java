81ddac9439f765052bb3235a921474f014d0856a
prat <pratiksh.patel91@gmail.com>
2018-04-17 13:29:18 -0500

    User class updated

--- Model/User.java
+++ Model/User.java
@@ -1,5 +1,28 @@
 package Model;
 
-public class User {
+import java.util.Scanner;
+
+public class User
+{
+	Scanner userInput = new Scanner(System.in);
+	String storedUsername, storedPassword;
+	int validLogin = 0;
+	int invalidLogin = -1;
+
+	do
+	{
+		System.out.print("Enter your username: ");
+		storedUsername = userInput.nextLine();
+		System.out.print("Enter your password: ");
+		storedPassword = userInput.nextLine();
+		invalidLogin++;
+	}while(!"pratiksh".equals(storedUsername)||!"prat1234".equals(storedPassword));
+
+	System.out.printf("\nWelcome %s; you have sucesfully logged into your account ",storedUsername);
+
+	validLogin++;System.out.printf("\nLogin History: InvalidAttempts = %d ; ValidAttempts = %d",invalidLogin,validLogin);
+
+	}
+
 
 }
